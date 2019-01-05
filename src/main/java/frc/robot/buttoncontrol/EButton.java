38a5432a075bb8009dc87ffcc427f2167bf4490c
calculusrobotics <42429019+calculusrobotics@users.noreply.github.com>
2019-01-04 21:49:42 -0700

    Add files via upload

--- src/main/java/frc/robot/buttoncontrol/EButton.java
+++ src/main/java/frc/robot/buttoncontrol/EButton.java
@@ -1,53 +1,181 @@
 package frc.robot.buttoncontrol;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import java.util.ArrayList;
 
-// TODO: consider extending JoystickButton instead of making an encapsulation class for it
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Custom Button class to use for mutually E-xclusive Buttons
+ */
 public class EButton {
-	private final Joystick CONTROLLER;
-	private final int ID;
-	private final JoystickButton BUTTON;
-	
-	
-	
-	
-	
-	public EButton(Joystick controller, int id) {
-		CONTROLLER = controller;
-		ID = id;
-		
-		BUTTON = new JoystickButton(CONTROLLER, ID);
-	}
-	
-	
-	
-	public int getID() {
-		return ID;
-	}
-	
-	public void whenPressed(ECommand cmd) {
-		cmd.setButtonID(ID);
-		cmd.setType(1);
-		
-		BUTTON.whenPressed(cmd);
-	}
-	
-	public void whenReleased(ECommand cmd) {
-		cmd.setButtonID(ID);
-		cmd.setType(-1);
-		
-		BUTTON.whenPressed(cmd);
-	}
-	
-	public void whileHeld(ECommand cmd) {
-		cmd.setButtonID(ID);
-		cmd.setType(1);
-		
-		BUTTON.whileHeld(cmd);
-	}
-	
-	public boolean get() {
-		return BUTTON.get();
-	}
+    private static int nextID = 0;
+    private final int ID;
+
+    // under the hood button
+    private JoystickButton button;
+    // name of the button (used for logging button stuffs)
+    // #TODO: should we log button "stuffs" history? (like presses/releases and command runnings)
+    private final String NAME;
+
+    // ECommands to run (under the correct circumstances)
+    private ECommand cmd_whenPressed;
+    private ECommand cmd_whileHeld;
+    private ECommand cmd_whenReleased;
+
+    private boolean prevState;
+
+    private int ltci;
+
+    private ArrayList<Integer> groupIDs;
+
+    private int releaseRequests;
+    private int trueRequests;
+
+
+
+    /**
+     * Create a new Exclusive Button
+     * 
+     * @param joystick
+     * @param id
+     * @param name Name of the EButton (used for logging button changes)
+     */
+    public EButton(GenericHID joystick, int id, String name) {
+        ID = nextID;
+        nextID++;
+
+        // create the under the hood WPILib button
+        button = new JoystickButton(joystick, id);
+
+        NAME = name;
+
+        prevState = false;
+
+        groupIDs = new ArrayList<Integer>();
+
+        trueRequests = 0;
+
+        ltci = -1;
+    }
+
+
+
+    public boolean getPrevState() {
+        return prevState;
+    }
+
+    public void setPrevState() {
+        prevState = button.get();
+    }
+
+
+
+
+
+    public void whenPressed(ECommand cmd) {
+        cmd_whenPressed = cmd;
+    }
+
+    public ECommand getWhenPressed() {
+        return cmd_whenPressed;
+    }
+
+    public void e_whenPressed(Command cmd) {
+        button.whenPressed(cmd);
+    }
+
+
+
+    public void whileHeld(ECommand cmd) {
+        cmd_whileHeld = cmd;
+    }
+
+    public ECommand getWhileHeld() {
+        return cmd_whileHeld;
+    }
+
+    public void e_whileHeld(Command cmd) {
+        button.whileHeld(cmd);
+    }
+
+
+
+    public void whenReleased(ECommand cmd) {
+        cmd_whenReleased = cmd;
+    }
+
+    public ECommand getWhenReleased() {
+        return cmd_whenReleased;
+    }
+
+    public void e_whenReleased(Command cmd) {
+        button.whenReleased(cmd);
+    }
+
+
+
+    public boolean get() {
+        return button.get();
+    }
+
+
+
+    public void addGroupID(int groupID) {
+        groupIDs.add(groupID);
+    }
+
+    public int getGroupLength() {
+        return groupIDs.size();
+    }
+
+    public int getGroupID(int i) {
+        return groupIDs.get(i);
+    }
+
+
+
+    public int getID() {
+        return ID;
+    }
+
+
+
+    public void setLTCI(int it) {
+        ltci = it;
+    }
+
+    public int getLTCI() {
+        return ltci;
+    }
+
+
+
+
+
+    public void requestRelease() {
+        releaseRequests++;
+    }
+
+    public int getReleaseRequests() {
+        return releaseRequests;
+    }
+
+
+
+    public void requestTrue() {
+        trueRequests++;
+    }
+
+    public int getTrueRequests() {
+        return trueRequests;
+    }
+
+
+
+    public void resetRequests() {
+        releaseRequests = 0;
+        trueRequests = 0;
+    }
 }
--- src/main/java/frc/robot/buttoncontrol/ECommand.java
+++ src/main/java/frc/robot/buttoncontrol/ECommand.java
@@ -3,26 +3,20 @@ package frc.robot.buttoncontrol;
 import edu.wpi.first.wpilibj.command.Command;
 
 public abstract class ECommand extends Command {
-	// group this command's button belongs to
-	protected ButtonGroup group;
-	// id of this command's button
-	protected int buttonID;
-	// type of this command, 1 = whenPressed/whileHeld, -1 = whenReleased
-	protected int type;
-	
-	public void setButtonID(int id) {
-		buttonID = id;
-	}
-	
-	public void setType(int type) {
-		this.type = type;
-	}
-	
-	protected void inGroup(String group) {
-		this.group = ButtonGroupGroup.getGroup(group);
-	}
-	
-	protected boolean shouldRun() {
-		return group.shouldRun(buttonID, type);
-	}
+    protected void execute() {}
+    protected void interrupted() {
+        end();
+    }
+
+    // even though they are not used here, this is a good place to keep this enum b/c it is related to Commands
+    public enum CommandType {
+        WHEN_PRESSED,
+        WHILE_HELD,
+        WHEN_RELEASED
+    }
+
+    public abstract void exec();
+    public void onInterrupted() {
+        end();
+    }
 }
